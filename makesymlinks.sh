e0f73f090f73d163a3a5c23ae1aa52132d1a4ab6
menor <josemenor@gmail.com>
2020-04-16 12:42:43 +0200

    fix git symlink script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -13,7 +13,7 @@ preztodir="${dotdir}/prezto"
 ########## ZSH
 
 # .zshenv needs to be on the root since then we load everything from there
-ln -s ${preztodir}/runcoms/zshenv ~/.zshenv 
+ln -sv ${preztodir}/runcoms/zshenv ~/.zshenv 
 
 # move .zsh dotfiles into .config
 mkdir ${configdir}/zsh
@@ -21,20 +21,19 @@ mkdir ${configdir}/zsh
 # zsh files we need to link
 zshfiles="zlogin zlogout zpreztorc zprofile zshrc"
 
-ln -s ${preztodir}/init.zsh ${configdir}/zsh/
+ln -sv ${preztodir}/init.zsh ${configdir}/zsh
 
 for file in $zshfiles; do
-    echo "Creating symlink to ${file} in .config/zsh directory."
-    ln -s ${preztodir}/runcoms/$file ${configdir}/zsh/.$file
+    ln -sv ${preztodir}/runcoms/$file ${configdir}/zsh/.$file
 done
 
 # also link the modules directory
-ln -s ${preztodir}/modules ${configdir}/zsh
+ln -sv ${preztodir}/modules ${configdir}/zsh
 
 ########## GIT
-ln -s ${dotdir}/git ${configdir}/git
+ln -sv ${dotdir}/git ${configdir}
 
 ########## KARABINER
 # This should run after karabiner installation sinc eit depends on the directory
 # being there, keep this in mind when creating the brew install file
-ln -s ${dotdir}/karabiner.json ${configdir}/karabiner
+ln -sfv ${dotdir}/karabiner.json ${configdir}/karabiner
