252b187995a5caa90103b5575a5925eb95d17d54
menor <josemenor@gmail.com>
2020-04-16 10:00:10 +0200

    feat: move zsh and git configs to ~/.config

--- gvimrc
+++
@@ -1,1 +0,0 @@
-t guifont=Inconsolata\ for\ Powerline\ Nerd\ Font\ Complete\ Mono:h16
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,57 +1,34 @@
 #!/bin/bash
 ############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-# Adapted from https://github.com/michaeljsmalley/dotfiles/edit/master/makesymlinks.sh
+# This script creates the needed symlinks from the home directory to dotfiles in ~/.dotfiles
 ############################
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="agignore vimrc vimrc.bundles gvimrc gitconfig gitignore tmux.conf"
-# list of files/folders to symlink in homedir
+dotdir=~/.dotfiles
+configdir=~/.config
+preztodir="${dotdir}/prezto"
 
-##########
+########## ZSH
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
+# .zshenv needs to be on the root since then we load everything from there
+ln -s ${preztodir}/runcoms/zshenv ~/.zshenv 
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
+# move .zsh dotfiles into .config
+mkdir ${configdir}/zsh
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+# zsh files we need to link
+zshfiles="zlogin zlogout zpreztorc zprofile zshrc"
+
+ln -s ${preztodir}/init.zsh ${configdir}/zsh/
+
+for file in $zshfiles; do
+    echo "Creating symlink to ${file} in .config/zsh directory."
+    ln -s ${preztodir}/runcoms/$file ${configdir}/zsh/.$file
 done
 
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        sudo apt-get install zsh
-        install_zsh
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
-}
+# also link the modules directory
+ln -s ${preztodir}/modules ${configdir}/zsh
 
-install_zsh
+########## TODO GIT VIM
