ce3739f05f57a2417714f089a80110ef53b13328
menor <josemenor@gmail.com>
2021-11-11 08:33:54 +0100

    semi working lua config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 -- Based on https://oroques.dev/notes/neovim-init/
+-- and https://gist.github.com/benfrain/97f2b91087121b2d4ba0dcc4202d252f
 -- You will need to install language servers
--- TODO: See if this can be auto
 -- npm i -g typescript typescript-language-server
 -- npm i -g vscode-langservers-extracted 
 
@@ -36,8 +36,10 @@ opt.signcolumn='number'
 -- as a vim command https://github.com/neovim/neovim/pull/12378
 cmd 'au CursorHold * checktime'
 
-opt.undofile = true                 -- use an undo file
-opt.undodir='~/.tmp/nvim/.vimundo/' -- set a directory to store the undo history
+-- use an undo file
+opt.undofile = true
+-- set a directory to store the undo history
+opt.undodir= os.getenv("HOME") .. '/.tmp/nvim/.vimundo/'
 
 opt.termguicolors = true
 
@@ -45,35 +47,7 @@ opt.termguicolors = true
 -- https://stackoverflow.com/a/67890996
 -- map('n', 'gx', cmd "execute 'silent! !open ' . shellescape(expand('<cWORD>'), 1)<cr>", {noremap = true, silent = true})
 
------------------------------------ plugins -----------------------------------
-
--- we use Paq as a package manager since it is written in Lua
--- we need to install it first https://github.com/savq/paq-nvim 
--- you can run :PaqSync to install, clean and update packages in one go
-require 'paq-nvim' {
-  'savq/paq-nvim',
-  '907th/vim-auto-save',
-  'christoomey/vim-tmux-navigator',   -- tmux & vim love
-  'tpope/vim-vinegar',
-  'tpope/vim-commentary',
-  'tpope/vim-surround',
-
-  -- for improved syntax highlighting, for how to install language parsers
-  -- check https://github.com/nvim-treesitter/nvim-treesitter#language-parsers
-  "nvim-treesitter/nvim-treesitter",
-
-  -- UI
-  'arcticicestudio/nord-vim',
-  'sainnhe/everforest',
-
-  -- autocompletion
-  'neovim/nvim-lspconfig', -- includes configs for most language servers
-  -- autocompe is what handles the autocompletion, it uses neovim lsp
-  -- underneath to accomplish that. Author is working on an improved version
-  -- of this plugin https://github.com/hrsh7th/nvim-cmp so that is something to
-  -- keep an eye on
-  'hrsh7th/nvim-compe'     
-}
+local plugins = require "plugins"
 
 ------------------------------------ theme -------------------------------------
 
@@ -85,44 +59,113 @@ g.nord_underline = 1
 g.nord_uniform_diff_background = 1
 g.nord_cursor_line_number_background = 1
 
--- everforest
-vim.g.everforest_background = "medium"
-vim.g.everforest_enable_italic = 1
-vim.g.everforest_diagnostic_text_highlight = 1
-vim.g.everforest_diagnostic_virtual_text = "colored"
-vim.g.everforest_current_word = "bold"
-
 cmd 'colorscheme nord'  
 
 -------------------------------- plugins config ---------------------------------
 -- vim autosave
-g.auto_save = 1           -- enable autosave on vim startup
-g.auto_save_silent = 1
+-- g.auto_save = 1           -- enable autosave on vim startup
+-- g.auto_save_silent = 1
+
+local autosave = require('autosave')
+
+autosave.setup(
+  {
+    enabled = true,
+    execution_message = '',
+    events = {'InsertLeave', 'TextChanged'},
+    conditions = {
+      exists = true,
+      modifiable = true,
+    },
+  }
+)
+
 
 -- treesitter
-local ts = require("nvim-treesitter.configs")
-ts.setup({
-  ensure_installed = "maintained",
-  highlight = { enable = true }
-})
+-- local ts = require("nvim-treesitter.configs")
+-- ts.setup({
+  -- ensure_installed = "maintained",
+  -- highlight = { enable = true }
+-- })
 
--- typescript
-require('lspconfig').tsserver.setup{}
+-- lspconfig
+-- https://blog.inkdrop.app/how-to-set-up-neovim-0-5-modern-plugins-lsp-treesitter-etc-542c3d9c9887
+local nvim_lsp = require('lspconfig')
 
--- nvim-compe
-opt.completeopt = "menuone,noselect"
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
 
-require('compe').setup({
-  enabled = true,
-  autocomplete = true,
-  source = {
-    path = true,
-    nvim_lsp = true,
-    nvim_lua = true,
-  },
-})
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+--   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+--   buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+-- --   buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+-- --   buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+-- --   buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+--   buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+-- --   buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+-- --   buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+-- --   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+-- --   buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+--   buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+--   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+-- --   buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+-- --   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
 
----------------------------------- keybindings -----------------------------------
+nvim_lsp.tsserver.setup {
+  on_attach = on_attach
+}
+
+-- Go pls
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md
+require'lspconfig'.gopls.setup{}
+
+-- nvim-cmp
+-- local cmp = require('cmp').setup({
+  -- sources = {
+    -- { name = 'buffer' },
+    -- { name = 'nvim_lsp' },
+    -- { name = 'path' },
+  -- }
+-- })
+
+-- The nvim-cmp almost supports LSP's capabilities
+-- so You should advertise it to LSP servers..
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- telescope
+map('n', '<leader>ff', ':Telescope find_files<CR>', {noremap = true, silent = true})
+map('n', '<leader>fa', ':Telescope file_browser<CR>', {noremap = true, silent = true})
+map('n', '<leader>fg', ':Telescope live_grep<CR>', {noremap = true, silent = true})
+
+require('telescope').setup{
+  defaults = {
+    layout_strategy = "vertical",
+    layout_config = {
+      vertical = {
+        mirror = true
+      }
+    }
+  }
+}
+
+-- autopairs
+-- require('nvim-autopairs').setup({})
+
+-- require("nvim-autopairs.completion.cmp").setup({
+  -- map_cr = true, --  map <CR> on insert mode
+  -- map_complete = true, -- it will auto insert `(` after select function or method item
+  -- auto_select = false,  -- auto select first item
+-- })
+
 -- moving through buffers, inspired by unimpaired.vim plugin
 map('n', '[b', ':bprevious<CR>', {noremap = true, silent = true})
 map('n', ']b', ':bnext<CR>', {noremap = true, silent = true})
@@ -139,4 +182,4 @@ map('n', 'tc', ':tabc<CR>', {noremap = true, silent = true})
 map('n', 'tC', ':tabo<CR>', {noremap = true, silent = true})
 
 -- Clear highlighted search
-map('n', '<leader>C', ':nohlsearch<CR>', {noremap = true, silent = true})
+map('n', '<leader>c', ':nohlsearch<CR>', {noremap = true, silent = true})
